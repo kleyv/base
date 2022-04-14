68fa6560d0c2de235a99653935fa3ecd73c5d37a
Eukleyv Cardoso <eukleyvcardoso@gmail.com>
2022-04-14 13:16:31 +0100

    Create project user story

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,20 @@ class ProjectsController < ApplicationController
     @project = Project.new
   end
 
+  def create
+    @project = Project.new(project_params)
+    @project.creator = current_user
+    if @project.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:name, :description)
+  end
+
 end
